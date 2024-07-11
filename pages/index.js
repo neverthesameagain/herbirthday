8ccbb8e44c19f282021b42e84aab40c3e542e14d
Aryan Mathur <126969847+neverthesameagain@users.noreply.github.com>
2024-07-11 16:56:32 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -25,16 +25,16 @@ export default function Home()
   return (
     <>
     
-      <div className="flex flex-row justify-center items-center w-full h-full bg-pink-200 p-10 drop-shadow-xl">
+      <div className="flex flex-row justify-center items-center w-full h-full bg-red-200 p-10 drop-shadow-xl">
         <div className="w-2/3 h-64 bg-gradient-to-r from-pink-300 to-pink-200 drop-shadow-xl rounded-full text-center transition-all duration-300">
-          <h1 className="p-20 tracking-wide text-8xl font-bold text-white uppercase drop-shadow-lg">Hi Beautiful</h1>
+          <h1 className="p-20 tracking-wide text-2l font-bold text-white uppercase drop-shadow-lg">nah like Krushi Finally reached the page, hmm, great work !</h1>
         </div>
       </div>
 
       <div className="flex flex-row items-center justify-evenly w-full h-96 bg-pink-50">
-        <CommentedImage src='/marina1.jpg' text="wow so pretty"/>
-        <CommentedImage src='/marina2.jpg' text="omg we&apos;re so cute"/>
-        <CommentedImage src='/marina3.jpg' text="we should kiss?"/>
+        <CommentedImage src='/marina1.jpg' text="Aww so pretty, how about a Rose?"/>
+        <CommentedImage src='/marina2.jpg' text="A chocolate for my heart?"/>
+        <CommentedImage src='/marina3.jpg' text="Should we kiss?"/>
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
@@ -43,19 +43,19 @@ export default function Home()
 
       <div className="flex flex-col items-center text-center w-full h-96 bg-pink-50 p-10">
         <div className="flex flex-row items-center text-center w-full h-5/6 bg-pink-50">
-          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>You Need</h1>
+          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>It is your birthday and</h1>
           <img src='https://sweezy-cursors.com/wp-content/uploads/cursor/milk-amp-mocha-bear-love/milk-mocha-bear-love-custom-cursor.png' width={600}/>
-          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>Someone For</h1>
+          <h1 className='w-1/3 text-pink-300 font-bold text-6xl'>Aryan planned some surprises</h1>
         </div>
-        <h1 className="w-full h-1/4 text-pink-300 font-bold text-6xl uppercase">prom</h1>
+        <h1 className="w-full h-1/4 text-pink-300 font-bold text-6xl uppercase">And you wanna know them right?</h1>
       </div>
 
       <div className="text-center w-full h-64 bg-pink-200 drop-shadow-xl">
-        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">So...</h1>
+        <h1 className="p-20 tracking-wide text-6xl font-bold text-pink-400 uppercase drop-shadow-xl">So... verify your identity first</h1>
       </div>
 
       <CornyQuestion 
-        question="Would you be the Elephant&apos;s Foot to my Chernobyl Disaster?"
+        question="Who are you?"
         nonactive="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Nuclear_symbol.svg/1024px-Nuclear_symbol.svg.png"
         active="https://creazilla-store.fra1.digitaloceanspaces.com/cliparts/76629/nuclear-explosion-clipart-xl.png"
         primary
@@ -71,7 +71,7 @@ export default function Home()
       </div>
 
       <CornyQuestion 
-        question="Would you be the butt to my Red Guy?"
+        question="When did we first meet?"
         nonactive="https://loudbooru.com/_images/a1c2fa54153d30da1045a967fe8fd8a9/9228%20-%20character%3Alincoln_loud%20character%3Athe_red_guy%20cow_and_chicken%20crossover%20devil%20dialogue%20satan.png"
         active="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/860bb581-65ee-4112-854f-1255c5579143/dffb3t8-d1a0991f-db5c-49ff-a7f2-2bc8d3278004.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzg2MGJiNTgxLTY1ZWUtNDExMi04NTRmLTEyNTVjNTU3OTE0M1wvZGZmYjN0OC1kMWEwOTkxZi1kYjVjLTQ5ZmYtYTdmMi0yYmM4ZDMyNzgwMDQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.F2pBVw7v1HfA3zLlC5V5lc1xberJTFDwLT_GoKlT7Vw"
         secondary
@@ -79,7 +79,7 @@ export default function Home()
       />
 
       <CornyQuestion 
-        question="Would you be the smut to my Among Us?"
+        question="What do you expect from Aryan?"
         nonactive="https://assets.stickpng.com/images/61d183263a856e0004c6334a.png"
         active="https://pbs.twimg.com/media/E2ieswGVgAYKWt3.jpg"
         primary
@@ -87,7 +87,7 @@ export default function Home()
       />
 
       <CornyQuestion 
-        question="Would you be the banana to my Minion?"
+        question="Would you be Aryan's Date?"
         nonactive="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/0a76b087-d923-4acf-85c5-118e6b81c7a0/d70lfsz-0a567aaf-a3ec-4cca-ac87-3820e4727e8f.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzBhNzZiMDg3LWQ5MjMtNGFjZi04NWM1LTExOGU2YjgxYzdhMFwvZDcwbGZzei0wYTU2N2FhZi1hM2VjLTRjY2EtYWM4Ny0zODIwZTQ3MjdlOGYucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.PZQsxdrOyzYABwsLATW4K94fX8RQBzUIzLrR-l8BDPU"
         active="https://i.redd.it/v0ep5v1vbl331.png"
         secondary
@@ -95,7 +95,7 @@ export default function Home()
       />
 
       <div className="flex flex-row justify-around items-center text-center w-full h-96 bg-pink-50 px-5 pt-5">
-        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>If I&apos;m C would you be my C++ to prom?</h1>
+        <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>If I&apos;m C would you be my C++ today?</h1>
       </div>
 
       <div className="flex flex-row justify-around items-start text-center w-full h-36 bg-pink-50 pb-5">
@@ -152,20 +152,20 @@ export default function Home()
       {
         confirmed ? 
         <div className="flex flex-col justify-around items-center text-center w-full h-[58em] bg-pink-200 p-5">
-          <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>Prom Confirmation Ticket / Review</h1>
+          <h1 className='w-2/3 text-pink-400 font-bold text-6xl'>So here is The Plan </h1>
           <div className="flex flex-col items-center gap-5 text-center w-3/4 h-5/6 bg-white p-5 rounded-3xl shadow-xl">
             <img className="h-24" src="https://cdn.pixabay.com/photo/2018/02/19/20/28/abstract-3166168__480.png" alt=""/>
             <h1 className='w-2/3 text-black font-bold text-4xl underline'>
               Thank You For Your Confirmation!
             </h1>
             <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
-              Name of Partner
+              Krushi
             </h2>
             <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
-              Aidan Ouckama
+              Aryan
             </h3>
             <h2 className='w-2/3 text-black opacity-75 font-bold italic text-2xl'>
-              Location
+              New Delhi
             </h2>
             <h3 className='w-2/3 text-black opacity-90 font-bold text-2xl'>
               Grove by the River, Newark
